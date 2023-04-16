d2b7f35b592b5c375a30a8a4329d75b31c28db04
Raphael @ dell-g15 <raphaelfv@poli.ufrj.br>
2023-04-16 17:12:27 -0300

    [master] (fix) /api sem get (pega tudo)

--- myproj/views.py
+++ myproj/views.py
@@ -8,6 +8,7 @@ from django.contrib import messages
 from django.views.generic.list import ListView
 import basedosdados as bd
 from .settings import GOOGLE_CLOUD_PROJECT_ID
+from django.core.serializers import serialize
 
 # def first(request):
 #     return render(request, 'first.html', {})
@@ -23,9 +24,12 @@ def api(request):
     df = bd.read_sql ( "SELECT * FROM `datario.transporte_rodoviario_municipal.gps_onibus` LIMIT 1000" , billing_project_id = GOOGLE_CLOUD_PROJECT_ID )
     if linha:
         df2=df.query("servico == '" + linha + "'")
-    if num_ordem:
+    elif num_ordem:
         df2 = df[df['id_veiculo'].str.contains(num_ordem)]
+    else:
+        df2=df
     result = df2.to_json(orient="records")
     parsed = json.loads(result)
 
     return JsonResponse(parsed,safe=False)
+    # return serialize("geojson",parsed, geometry_field="point",)# fields=["name"])
